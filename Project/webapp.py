bbea13b6183d94cd4f25ae42e236ade1dd292abb
N-eeraj <70063961+N-eeraj@users.noreply.github.com>
2021-05-17 12:34:53 +0530

    Update webapp.py

--- Project/webapp.py
+++ Project/webapp.py
@@ -8,18 +8,42 @@ import pandas as pd
 app = Flask(__name__)
 app.secret_key = 'xyz'
 
+### defining dataframe/db
+user_db = pd.read_csv(r'database/user_db.csv')
+event_db = pd.read_csv(r'database/event_db.csv')
+print(user_db)
+print(event_db)
+
+
+### defining routes
+#login page route
 @app.route('/')
 def main():
     return render_template('login.html')
 
+#login function route
 @app.route('/login', methods = ['post'])
 def login():
-    login_email = request.form['login_email']
-    login_pswd = request.form['login_pswd']
+    login_email = request.form['login-email']
+    login_pswd = request.form['login-pswd']
+    print(login_email, login_pswd)
     return '''<script>alert('Logging In');window.location='/'</script>'''
 
+#sign up page route
 @app.route('/sign_up')
+def signup():
+    return render_template('sign_up.html')
+
+#sign up funtion route
+@app.route('/signing_up', methods = ['post'])
 def signing_up():
+    signup_fname = request.form['sign-up_fname']
+    signup_lname = request.form['sign-up_lname']
+    signup_email = request.form['sign-up_email']
+    signup_phone = request.form['sign-up_phone']
+    signup_pswd1 = request.form['sign-up_pswd-1']
+    signup_pswd2 = request.form['sign-up_pswd-2']
+    print(signup_fname + ' ' + signup_lname, signup_email, signup_phone, signup_pswd1, signup_pswd2)
     return '''<script>alert('Signing Up');window.location='/sign_up'</script>'''
 
 if __name__ == '__main__':
