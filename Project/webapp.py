5105c012ffaec557daa9427720bc70231a2b518a
N-eeraj <neeraj7rajeena@gmail.com>
2021-05-18 15:56:42 +0530

    validating used mail & phone

--- Project/webapp.py
+++ Project/webapp.py
@@ -36,18 +36,20 @@ def signup():
 #sign up funtion route
 @app.route('/signing_up', methods = ['post'])
 def signing_up():
+    global user_db
     signup_fname = request.form['sign_up-fname']
     signup_lname = request.form['sign_up-lname']
     signup_email = request.form['sign_up-email']
     signup_phone = request.form['sign_up-phone']
     signup_pswd1 = request.form['sign_up-pswd-1']
     signup_pswd2 = request.form['sign_up-pswd-2']
+    if signup_email in np.array(user_db.email):
+        return '''<script>alert('This Email is already in use');window.location='/sign_up'</script>'''
+    if signup_phone in np.array(user_db.phone):
+        return '''<script>alert('This Phone Number is already in use');window.location='/sign_up'</script>'''
     if signup_pswd1!=signup_pswd2:
         return '''<script>alert('Passwords not Matching');window.location='/sign_up'</script>'''
     elif signup_pswd1==signup_pswd2:
-        global user_db
-        if (signup_email in np.array(user_db.email)):
-            '''<script>alert('Signing Up');window.location='/sign_up'</script>'''
         if user_db.login_id.max()!=True and user_db.login_id.max()!=0:
             next_uid = 0
         else:
@@ -56,10 +58,8 @@ def signing_up():
                 'phone': signup_phone, 'password': signup_pswd1, 'profile_pic': 'None',
                 'events_reg': [], 'events_atnd': [], 'events_host': []}]
         user_db = user_db.append(data, ignore_index=True, sort=False)
-        print(user_db)
-        # user_db.to_csv('database/user_db.csv', index=False)
-        # user_db = pd.read_csv('database/user_db.csv')
-        # print(user_db)
+        user_db.to_csv('database/user_db.csv', index=False)
+        user_db = pd.read_csv('database/user_db.csv')
         return '''<script>alert('Signing Up');window.location='/'</script>'''
     else:
         return '''<script>alert('Error');window.location='/sign_up'</script>'''
